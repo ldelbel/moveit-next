0d523c15661e0b003e0483fbe2a5b991d12bffe9
ldelbel <lucdelbel@gmail.com>
2021-02-25 10:21:16 -0300

    Add styles

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../styles/components/Profile.module.css'
+
+const Profile = () => {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/ldelbel.png" alt="Lucas Delbel" />
+      <div>
+        <strong>Lucas Delbel</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
